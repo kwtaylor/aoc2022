9242430f1d0af90b79de722e9636c8bbd569cbd6
Ken Taylor <taylork@alum.mit.edu>
2022-12-12 00:42:39 -0800

    Faster Day 12 part 2 (after spoilers) + visualization

--- p12/p12.py
+++ p12/p12.py
@@ -30,6 +30,8 @@ width = len(m[0])
 #heapq with elements:
 #(estcost, steps, (r,c))
 q = []
+#visited holds None if not visited
+#(steps, previous) if visited
 visited = []
 
 def dist(pta, ptb):
@@ -38,36 +40,40 @@ def dist(pta, ptb):
 def add_neighbors(pt, steps):
     (r,c) = pt
     cur_h = m[r][c]
+    newstep = steps+1
     for n in (r-1,c),(r+1,c),(r,c-1),(r,c+1):
         (nr, nc) = n
         if nr<0 or nc<0 or nr>=height or nc>=width:
             #out of bounds
             continue
-        elif visited[nr][nc]:
-            #visited
+        vis = visited[nr][nc]
+        if vis and vis[0] <= newstep:
+            #visited path is shorter
             continue
         elif m[nr][nc] - m[r][c] > 1:
             #can't reach
             continue
         else:
             #add to heap!
-            cost = steps + 1 + dist(n,end)
-            heapq.heappush(q,(cost, steps+1, n))
-            visited[nr][nc] = True
+            cost = newstep + dist(n,end)
+            heapq.heappush(q,(cost, newstep, n))
+            visited[nr][nc] = (newstep, pt)
 
-def find_short(st):
+def find_short(sts):
     #clear out queue
     global q, visited
     q = []
-    visited = [[False]*width for _ in range(height)]
+    visited = [[None]*width for _ in range(height)]
     
-    #add starting point
-    add_neighbors(st, 0)
-    visited[st[0]][st[1]] = True
+    #add starting point(s)
+    for st in sts:
+        heapq.heappush(q,(dist(st,end), 0, st))
+        visited[st[0]][st[1]] = (0, st)
 
     #iterate
     while len(q) > 0:
         spot = heapq.heappop(q)
+        #print(spot)
         spotstep = spot[1]
         spotpt = spot[2]
         if spot[2] == end:
@@ -77,17 +83,28 @@ def find_short(st):
 
     return None
 
-#part 1
-print(find_short(start))
+def print_path(endpt):
+    pmap = []
+    for row in m:
+        prow = list(map(chr, row))
+        pmap.append(prow)
 
-trails = []
+    pt = endpt
+    while True:
+        pmap[pt[0]][pt[1]] = "*"
+        prev = visited[pt[0]][pt[1]][1]
+        if prev==None or prev==pt:
+            break
+        else:
+            pt = prev
+    for row in pmap:
+        print("".join(row))
+
+#part 1
+print(find_short([start]))
+print_path(end)
 
 #part 2
-for pt in eleva:
-    cost = find_short(pt)
-    #print(pt, cost)
-    if cost != None:
-        trails.append(cost)
-
-print(min(trails))
+print(find_short(eleva))
+print_path(end)
 
