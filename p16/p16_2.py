c49e9d7bbc75c165d24d50a5deb96141e822f513
Ken Taylor <taylork@alum.mit.edu>
2022-12-18 00:55:47 -0800

    Day 16 Part 2 solved!

--- p16/p16_2.py
+++ p16/p16_2.py
@@ -4,68 +4,75 @@ from collections import deque
 #values are [flow, [lead_to]]
 valves = dict()
 
-for l in open("example"):
+for l in open("input"):
     m = re.match(r"Valve (..) has flow rate=(\d+); .* valves? (.*)", l)
     v = m[1]
     r = int(m[2])
     to = re.findall(r"[A-Z]+",m[3])
     valves[v] = [r,to]
 
-#state is (move, score, ele_score, cur_valve, ele_valve, (enabled_valves), {valve:best_score}, {ele_valve:best_score})
+print(valves)
+
+def get_cost(start_valve):
+    cost_to = dict()
+    queue = deque()
+    queue.append(start_valve)
+    cost_to[start_valve] = 0
+    while queue:
+        v = queue.popleft()
+        c=cost_to[v]
+        for nv in valves[v][1]:
+            if nv not in cost_to:
+                cost_to[nv] = c+1
+                queue.append(nv)
+    return cost_to
+
+#calculate cost to nodes, only for "good valves" and start point, to create a smaller network
+#values are {lead_to:cost}
+good_valves = dict()
+START_VALVE = 'AA'
+
+for vn,v in valves.items():
+    if v[0] > 0 or vn==START_VALVE:
+        good_valves[vn] = dict()
+        costs = get_cost(vn)
+        for cn,c in costs.items():
+            if valves[cn][0] > 0:
+                good_valves[vn][cn]=c
+
+print(good_valves)
+
+#state is (move, score, cur_valve, dist, ele_valve, ele_dist, (enabled_valves))
 search_states = deque()
-search_states.append((1,0,0,'AA','AA',(),{},{}))
+if valves[START_VALVE][0] == 0:
+    #if broken valve at start, mark as enabled to simplify serach
+    e = (START_VALVE,)
+else:
+    e = tuple()
+search_states.append((1,0,START_VALVE,0,START_VALVE,0,e))
 MAX_MOVE=26
 best_score=0
 best_possible=0
 best_valves = ()
 culled=0
 
-def best_possible_score(score,move,valve,e_valve,enabled_valves,ele=False):
-    if move >= MAX_MOVE:
-        return score
-    valve_val=valves[valve][0]
-    openable = valve_val > 0 and valve not in enabled_valves and (not ele or e_valve != valve)
-    e_valve_val=valves[e_valve][0]
-    e_openable = e_valve_val > 0 and e_valve not in enabled_valves and (ele or e_valve != valve)
+def best_possible_score(score,move,enabled_valves):
     rates = []
     for vn,v in valves.items():
-        if v[0] > 0 and vn not in enabled_valves and vn != valve and vn != e_valve:
+        if v[0] > 0 and vn not in enabled_valves:
             rates.append(v[0])
     rates.sort(reverse=True)
-    m=move+1 if ele else move
-    e_m=move
-
-    #if user/elephant are in a node with a usable valve, assume they open it first
-    if openable:
-        score += valve_val*(MAX_MOVE-m)
-        m+=2
-    else:
-        m+=1
-    if e_openable:
-        score += e_valve_val*(MAX_MOVE-e_m)
-        e_m+=2
-    else:
-        e_m+=1
-    
+    m=move
+    if m>=MAX_MOVE:
+        return score
     for r in range(0,len(rates),2):
         #simulate 2 simultaneous valve openings
-        if ele:
-            score += rates[r]*(MAX_MOVE-e_m)
-            if r+1<len(rates) and m<MAX_MOVE:
-                score += rates[r+1]*(MAX_MOVE-m)
-            e_m+=2
-            m+=2
-            if e_m>=MAX_MOVE:
-                break
-        else:
-            score += rates[r]*(MAX_MOVE-m)
-            if r+1<len(rates) and e_m<MAX_MOVE:
-                score += rates[r+1]*(MAX_MOVE-e_m)
-            e_m+=2
-            m+=2
-            if m>=MAX_MOVE:
-                break
-        
+        score += rates[r]*(MAX_MOVE-m)
+        if r+1<len(rates):
+            score += rates[r+1]*(MAX_MOVE-m)
+        m+=2
+        if m>=MAX_MOVE:
+            break
     return score
 
 def get_moves(state,ele=False):
@@ -73,61 +80,75 @@ def get_moves(state,ele=False):
     global best_valves
     global best_possible
     global culled
-    m,s,es,v,ev,e,bs,ebs=state
+    m,s,v,d,ev,ed,e=state
 
     #determine best possible score by imagining this node is connected
     #to all other unopened ones in a perfect sequence
-    best_possible=best_possible_score(s+es,m,v,ev,e,ele)
-        
+    best_possible=best_possible_score(s,m,e)
+
     if best_possible <= best_score:
         #print("cull due to best possible score",b,"less than best score",best_score)
         culled+=1
         return []
 
     if ele:
-        ov=v
-        obs=bs
-        v=ev
-        bs=ebs
-        os=s
-        (s,es)=(es,s)
+        ev,v=v,ev
+        ed,d=d,ed
 
-    if v not in bs:
-        bs=dict(bs)
-        bs[v]=s
-        
-    new_states=[]
+    #move forward toward destination nodes
+    if d>0:
+        if d>ed:
+            #other side reaches destination first
+            d-=ed+1
+            m+=ed
+            if ele:
+                if m+1 >= MAX_MOVE-1:
+                    return []
+                else:
+                    return [(m+1,s,ev,0,v,d,e)]
+            else:
+                if m >= MAX_MOVE-1:
+                    return []
+                else:
+                    return [(m,s,v,d,ev,0,e)]
+        else:
+            #we reach destination, continue below
+            ed-=d
+            m+=d
+
+    #actions during last minute don't affect score, so cull here
+    if m>=MAX_MOVE-1:
+        return []
+    
     valve_val=valves[v][0]
     openable = valve_val > 0 and v not in e
-    
+
+    #if openable, always open
     if openable:
         #enable valve (only if non-zero), update best score
         open_score=valve_val*(MAX_MOVE-m)
         new_valves=e+(v,)
         ns=s+open_score
-        if es+ns >= best_score:
-            best_score = es+ns
+        if ns >= best_score:
+            best_score = ns
             best_valves = new_valves
-        nbs=dict(bs)
-        nbs[v]=ns
         if ele:
-            new_states.append((m+1,os,ns,ov,v,new_valves,obs,nbs))
+            return [(m+1,ns,ev,ed,v,0,new_valves)]
         else:
-            new_states.append((m,ns,es,v,ev,new_valves,nbs,ebs))
+            return [(m,ns,v,0,ev,ed,new_valves)]
 
-    #actions during mintue 30 don't affect score, so cull here
-    if m==MAX_MOVE-1:
-        return []
-    
-    for n in valves[v][1]:
-        #move to valve (only if score has improved since last visit)
-        if n in bs and s<=bs[n]:
+    #else move to another openable valve
+    new_states=[]
+        
+    for n,c in good_valves[v].items():
+        #move to next openable valve
+        if n in e:
             culled+=1
         else:
             if ele:
-                new_states.append((m+1,os,s,ov,n,e,obs,bs))
+                new_states.append((m+1,s,ev,ed,n,c-1,e))
             else:
-                new_states.append((m,s,es,n,ev,e,bs,ebs))
+                new_states.append((m,s,n,c-1,ev,ed,e))
     
     return new_states
 
